cbc67d3a9beab519f6b7b93d00f8405c03af944a
e-felix <42306143+e-felix@users.noreply.github.com>
2024-11-01 19:28:52 +0100

    [RED] Can update command cmd

--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -17,3 +17,19 @@ func TestNewCommand(t *testing.T) {
 		t.Fatalf("Found %v, expected %v", command, expected)
 	}
 }
+
+func TestCommandUpdateCmd(t *testing.T) {
+	cmd := "echo"
+	args := make([]string, 0)
+	args = append(args, "Hello")
+
+	newCmd := "ls"
+	expected := &Command{Cmd: newCmd, Args: args}
+
+	command := &Command{Cmd: cmd, Args: args}
+	command.UpdateCmd(newCmd)
+
+	if !reflect.DeepEqual(command, expected) {
+		t.Fatalf("Found %v, expected %v", command, expected)
+	}
+}
